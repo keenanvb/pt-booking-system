ea74d8e30b17146e846c7e06a319ca760a0797b3
Keenanvb <keenanvanblerk@gmail.com>
2020-12-27 12:55:38 +0200

    update

--- client/src/components/layout/landing/landing-sidenav/sidebar.js
+++ client/src/components/layout/landing/landing-sidenav/sidebar.js
@@ -1,40 +1,38 @@
-import React, { useState } from "react"
-import { Link } from 'react-router-dom'
-import "./componentStyles.css"
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './componentStyles.css';
 
 const Sidebar = ({ sidebar, close }) => {
+	const [sidebarClass, setSidebarClass] = useState(sidebar);
 
-    const [sidebarClass, setSidebarClass] = useState(sidebar)
+	let closeHandler = (e) => {
+		e.preventDefault();
+		setSidebarClass('sidebar close');
+		setTimeout(() => {
+			close();
+		}, 1000);
+	};
 
-    let closeHandler = (e) => {
-        e.preventDefault();
-        setSidebarClass('sidebar close');
-        setTimeout(() => {
-            close();
-        }, 1000)
-    }
+	return (
+		<div className={sidebarClass}>
+			<button className="close" style={{ color: 'red' }} onClick={closeHandler}>
+				<i className="fa fa-times"></i>
+			</button>
+			<div className="account">
+				<ul onClick={closeHandler}>
+					<li>Under construction</li>
+				</ul>
+			</div>
+			<div className="side-nav-footer-social">
+				<a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
+					<i style={{ color: '#fb3958' }} className="fab fa-instagram fa-2x" />
+				</a>
+				<a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer">
+					<i style={{ color: '#0e76a8' }} className="fab fa-linkedin fa-2x" />
+				</a>
+			</div>
+		</div>
+	);
+};
 
-    return (
-        <div className={sidebarClass}>
-            <button className="close" style={{ color: 'red' }} onClick={closeHandler}>
-                <i className="fa fa-times"></i>
-            </button>
-            <div className='account'>
-                <ul onClick={closeHandler}>
-                    <li>Under construction</li>
-                </ul>
-            </div>
-            <div className="side-nav-footer-social">
-                <a href='https://www.instagram.com/lauren.alyssa_pt' target='_blank' rel='noopener noreferrer'>
-                    <i style={{ color: '#fb3958' }} className='fab fa-instagram fa-2x' />
-                </a>
-                <a href='https://www.linkedin.com/in/lauren-arries-228083122/' target='_blank' rel='noopener noreferrer'>
-                    <i style={{ color: '#0e76a8' }} className='fab fa-linkedin fa-2x' />
-                </a>
-
-            </div>
-        </div>
-    )
-}
-
-export default Sidebar
+export default Sidebar;
